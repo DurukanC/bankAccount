8725778ccacd203e3c788b5d08cc67a1667a2aae
Durukan Cetin <durukan.cetinn@gmail.com>
2023-03-17 02:00:04 +0300

    bankAccount
    user's account information

---
+++ bankAccount.cpp
@@ -0,0 +1,16 @@
+﻿#include "bankAccount.h"
+
+int main() {
+
+
+	bankAccount acc1("Durukan", "Cetin", 3500, "05071234567");
+	cout << acc1;
+
+	acc1 + 10000;
+	cout << acc1;
+
+	acc1 - 20000;
+	cout << acc1;
+
+	return 0;
+}
